4f3574bd630ab787b981b13a735277ae69d7fe5c
andrey nikiforov <nikifortis@mail.ru>
2022-11-23 22:43:32 +0300

    я закопался...отстаю нихера не понимаю..

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    // Задание 1
+    int clientOS = 0;
+
+    {
+        if (clientOS == 0) {
+            System.out.println("Установите версию IOS");
+        } else {
+            System.out.println("Установите для андроид");
+            // Задание 2
+        }
+        int clientOS = 0;
+        int clientDeviseYear = 2020;
+
+        if (clientOS == 0) {
+            System.out.println("Облегчённая версия");
+        } else {
+            System.out.println("Установите версию для IOS");
+        }
+        if (clientOS == 0) {
+            System.out.println("Облегчённая версия Android");
+        } else {
+            System.out.println("Установите версию для Android");
+        }
+    }
+}
+
+
+
+
