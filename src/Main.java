c80e0cefc4da599e2541193f9f223021fb70e5f7
andrey nikiforov <nikifortis@mail.ru>
2022-11-23 23:00:43 +0300

    я закопался...отстаю нихера не понимаю..

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
             System.out.println("Облегчённая версия Android");
         } else {
             System.out.println("Установите версию для Android");
+            // Задание 3
         }
     }
 }
